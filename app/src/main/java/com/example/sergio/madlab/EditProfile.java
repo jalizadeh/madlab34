64b7a1091db887776f87256b68e9727d7e294ec2
JAlizadeh <jalizade3d@yahoo.com>
2018-04-23 20:03:11 +0200

    very first commit

---
+++ app/src/main/java/com/example/sergio/madlab/EditProfile.java
@@ -0,0 +1,88 @@
+package com.example.sergio.madlab;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EditProfile extends AppCompatActivity {
+
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit_profile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void saveData(MenuItem menuItem) {
+        Intent intent = new Intent(this, ViewProfile.class);
+        EditText editText_name = (EditText) findViewById(R.id.editText_name);
+        String name = editText_name.getText().toString();
+        intent.putExtra("name", name);
+        EditText editText_mail = (EditText) findViewById(R.id.editText_mail);
+        String mail = editText_mail.getText().toString();
+        intent.putExtra("mail", mail);
+        EditText editText_bio = (EditText) findViewById(R.id.editText_bio);
+        String bio = editText_bio.getText().toString();
+        intent.putExtra("bio", bio);
+        startActivity(intent);
+    }
+
+    public void openGallery(View view) {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && null != data) {
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imageView3);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                                .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sergio/madlab/ViewProfile.java
@@ -0,0 +1,77 @@
+package com.example.sergio.madlab;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewProfile extends AppCompatActivity {
+
+    private String name;
+    private String mail;
+    private String bio;
+    //private ImageView image;
+
+
+
+
+/*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_view_profile);
+        setContentView(R.layout.test);
+    }
+*/
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+        //setContentView(R.layout.test);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+        mail = intent.getStringExtra("mail");
+        bio = intent.getStringExtra("bio");
+
+        TextView textView_name = findViewById(R.id.name_text);
+        textView_name.setText(name);
+        TextView textView_mail = findViewById(R.id.mail_text);
+        textView_mail.setText(mail);
+        TextView textView_bio = findViewById(R.id.bio_text);
+        textView_bio.setText(bio);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString("name", name);
+        outState.putString("mail", mail);
+        outState.putString("bio", bio);
+        super.onSaveInstanceState(outState);
+        //TODO does not work
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_view_profile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void editProfile(MenuItem menuItem) {
+        Intent intent = new Intent(this, EditProfile.class);
+        startActivity(intent);
+    }
+
+
+}
