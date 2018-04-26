99eab59aee6500f1f5b771bfa81e651ebc4a31cf
JAlizadeh <jalizade3d@yahoo.com>
2018-04-26 11:21:16 +0200

    insertBook added

--- app/src/main/java/com/example/sergio/madlab/ViewProfile.java
+++ app/src/main/java/com/example/sergio/madlab/ViewProfile.java
@@ -6,16 +6,20 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.*;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.FileInputStream;
 
@@ -26,6 +30,8 @@ public class ViewProfile extends AppCompatActivity {
     private String bio;
     //private ImageView image;
 
+    private DatabaseReference mDatabase;
+    private static final String TAG = "DatabaseFact";
 
 
 
@@ -37,14 +43,6 @@ public class ViewProfile extends AppCompatActivity {
         setContentView(R.layout.test);
     }
 */
-
-
-    // Write a message to the database
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("https://madlab34-b64ac.firebaseio.com");
-
-
-    myRef.setValue("Hello, World!");
 
 
 
@@ -68,23 +66,32 @@ public class ViewProfile extends AppCompatActivity {
         TextView textView_bio = findViewById(R.id.bio_text);
         textView_bio.setText(bio);
 
-        /*
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        database.getReference("https://madlab34-b64ac.firebaseio.com/my/data");
-        */
+        Button btn_ib = findViewById(R.id.btn_ib);
 
-        /*
-        FileInputStream serviceAccount =
-                new FileInputStream("path/to/serviceAccountKey.json");
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://madlab34-b64ac.firebaseio.com")
-                .build();
+/*
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        //mDatabase.setValue("Hello, World!");
 
-        FirebaseApp.initializeApp(options);
+        // Read from the database
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
         */
     }
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
@@ -106,5 +113,9 @@ public class ViewProfile extends AppCompatActivity {
         startActivity(intent);
     }
 
-
+    public void openInsertBook(View view)
+    {
+        Intent intent = new Intent(this, InsertBook.class);
+        startActivity(intent);
+    }
 }
