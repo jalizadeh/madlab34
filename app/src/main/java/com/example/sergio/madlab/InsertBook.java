a74542f5cdccf120373796986a1400c883201b91
JAlizadeh <jalizade3d@yahoo.com>
2018-04-27 09:06:45 +0200

    book insertion + activity titles fixed

--- app/src/main/java/com/example/sergio/madlab/InsertBook.java
+++ app/src/main/java/com/example/sergio/madlab/InsertBook.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -19,8 +22,9 @@ import com.google.firebase.database.ValueEventListener;
 
 public class InsertBook extends AppCompatActivity {
 
+    /*
     //retrieved data from database
-    private long dbISBN=0;
+    private String dbISBN="";
     private String dbTitle="";
     private String dbAuthor="";
     private String dbPublisher="";
@@ -28,9 +32,25 @@ public class InsertBook extends AppCompatActivity {
     private String dbGenre="";
     private String dbTags="";
 
+    */
+
+    private String isbn="";
+    private String title="";
+    private String author="";
+    private String publisher="";
+    private String editYear= "";
+    private String genre="";
+    private String tags="";
+
+
     private static int RESULT_LOAD_IMG = 1;
-    private EditText edit1;
-    private EditText editt2;
+    private EditText etISBN;
+    private EditText etTitle;
+    private EditText etAuthor;
+    private EditText etPublisher;
+    private EditText etEditYear;
+    private Spinner spGenre;
+    private EditText etTags;
 
     private DatabaseReference databaseIB;
 
@@ -39,6 +59,15 @@ public class InsertBook extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_book);
 
+        etISBN = (EditText) findViewById(R.id.editText_book_isbn);
+        etTitle = (EditText) findViewById(R.id.editText_book_title);
+        etAuthor = (EditText) findViewById(R.id.editText_book_author);
+        etPublisher = (EditText) findViewById(R.id.editText_book_publisher);
+        etEditYear = (EditText) findViewById(R.id.editText_book_editYear);
+        spGenre = (Spinner) findViewById(R.id.spinner_bookGenre);
+        etTags = (EditText) findViewById(R.id.editText_book_tags);
+
+        /*
         // read from Books
         databaseIB = FirebaseDatabase.getInstance().getReference("Books");
         databaseIB.addValueEventListener(new ValueEventListener() {
@@ -52,11 +81,11 @@ public class InsertBook extends AppCompatActivity {
                 //Toast.makeText(getApplicationContext(),dbName+'-'+dbEmail+'-'+dbBio,Toast.LENGTH_LONG).show();
                 Toast.makeText(getApplicationContext(),dbISBN+"-"+dbTitle,Toast.LENGTH_SHORT).show();
 
-                /*
-                textView_name.setText(dbName);
-                textView_mail.setText(dbEmail);
-                textView_bio.setText(dbBio);
-                */
+
+                //textView_name.setText(dbName);
+                //textView_mail.setText(dbEmail);
+                //textView_bio.setText(dbBio);
+
             }
 
             @Override
@@ -65,6 +94,7 @@ public class InsertBook extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"db fetch failed",Toast.LENGTH_SHORT).show();
             }
         });
+        */
 
 
         /*
@@ -87,6 +117,42 @@ public class InsertBook extends AppCompatActivity {
     public void openGallery(View view) {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit_profile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void saveData(MenuItem menuItem) {
+
+        //prepare data to be stored
+        // may change in future: input validity is not checked
+        isbn = etISBN.getText().toString();
+        title = etTitle.getText().toString();
+        author = etAuthor.getText().toString();
+        publisher = etPublisher.getText().toString();
+        editYear = etEditYear.getText().toString();
+        genre = spGenre.getSelectedItem().toString();
+        tags = etTags.getText().toString();
+
+
+        //create a new child with isbn as its unique ID
+        databaseIB = FirebaseDatabase.getInstance().getReference("Books").child(isbn);
+        databaseIB.child("title").setValue(title);
+        databaseIB.child("author").setValue(author);
+        databaseIB.child("publisher").setValue(publisher);
+        databaseIB.child("edityear").setValue(editYear);
+        databaseIB.child("genre").setValue(genre);
+        databaseIB.child("tags").setValue(tags);
+
+        //show all input
+        //Toast.makeText(this, isbn+"\n"+title+"\n"+author+"\n"+publisher+"\n"+editYear+"\n"+genre+"\n"+tags,Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "Book saved successfully!",Toast.LENGTH_SHORT).show();
+
+        finish();
     }
 
 }
---
+++ app/src/main/java/com/example/sergio/madlab/ShowAllBooks.java
@@ -0,0 +1,8 @@
+package com.example.sergio.madlab;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ShowAllBooks extends AppCompatActivity{
+
+
+}
