782bc5d60a647d2ac0867fd2b507f25938778589
Sergio Aula <aula.100424@e.unavarra.es>
2018-06-13 01:20:32 +0200

    EditProfile location working

--- app/src/main/java/com/example/sergio/madlab/EditProfile.java
+++ app/src/main/java/com/example/sergio/madlab/EditProfile.java
@@ -30,11 +30,14 @@ import android.widget.Toast;
 import com.example.sergio.madlab.Classes.User;
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -143,6 +146,10 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
         btnOpenCamera.setOnClickListener((View.OnClickListener) this);
 
         profileImage = (ImageView)findViewById(R.id.imageView);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
 
 
@@ -335,6 +342,18 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(chosenLocation, 12.0f));
         }
 
+    }
+
+    public void chooseLocation(View view) {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
     }
 
 
